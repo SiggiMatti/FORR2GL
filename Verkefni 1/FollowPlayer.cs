4de3ba88cb67e93c5a66b76ac4303dd5d37c913f
SiggiMatti <54663423+SiggiMatti@users.noreply.github.com>
2021-03-20 17:50:41 +0000

    kóði fyrir verkefni 1

---
+++ Verkefni 1/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Public breyta sem tekur við bílnum
+    public GameObject player;
+    // Private breyta sem heldur bara um stöðu myndavélarinnar út frá bílnum
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Finn stöðu bílsins sem byrjunarpunkt og færi síðan myndavélina fyrir aftan hann með offset breytunni
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Verkefni 1/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Private breytur sem sjást aðeins í kóðanum hér. Þær halda utan um hraða.
+    private float speed = 5.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Stert er kallað áður en fyrsta uppfærsla skjásins
+    void Start()
+    {
+        
+    }
+
+    // Update er kallað á hverjum frame
+    void Update()
+    {
+        // Hér fæ ég innslátt frá notanda
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Hreyfi bílinn áfram 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Sný bílnum
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
